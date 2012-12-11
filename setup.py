b6d472e0ca120f153634b1ae23666f37a7af6710
root <root@localhost.(none)>
2012-12-11 12:07:53 +0000

    Add support for appending to files, including before or after specified placeholders

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@
 from distutils.core import setup
 
 setup(name='Instantly',
-      version='0.1',
-      description='A drag and drop interface to create WebElement templates quickly and easily.',
+      version='0.2',
+      description='A tool which allows you to create templates that can be expanded into full projects or stubs instantly.',
       author='Timothy Crosley',
       author_email='timothy.crosley@gmail.com',
       url='http://www.simpleinnovation.org/',
-      download_url='https://github.com/timothycrosley/Instantly/blob/master/dist/Instantly-0.1.tar.gz?raw=true',
+      download_url='https://github.com/timothycrosley/Instantly/blob/master/dist/Instantly-0.2.tar.gz?raw=true',
       license = "GNU GPLv2",
       scripts=['scripts/instantly',],
       requires = ['configobj', 'rest'],
