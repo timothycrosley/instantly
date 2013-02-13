b23bde79ec4c7dcf3aa808264725bb79dc8ef3b4
Timothy Crosley <timothy.crosley@gmail.com>
2013-02-12 21:08:22 -0500

    Instantly 0.5.0: ability to instantly add directories, switched to semantic versioning, python3 support

--- setup.py
+++ setup.py
@@ -3,15 +3,16 @@
 from distutils.core import setup
 
 setup(name='Instantly',
-      version='0.4',
+      version='0.5.0',
       description='A tool which allows you to create templates that can be expanded into full projects or stubs instantly.',
       author='Timothy Crosley',
       author_email='timothy.crosley@gmail.com',
       url='http://www.simpleinnovation.org/',
-      download_url='https://github.com/timothycrosley/Instantly/blob/master/dist/Instantly-0.4.tar.gz?raw=true',
+      download_url='https://github.com/timothycrosley/Instantly/blob/master/dist/Instantly-0.5.0.tar.gz?raw=true',
       license = "GNU GPLv2",
       scripts=['scripts/instantly',],
-      requires = ['configobj', 'rest'],
+      requires = ['configobj', 'rest', 'webelements'],
+      install_requires=['configobj', 'rest', 'webelements'],
       package_data={'InstantlyFiles': ["instant_templates/create_instant_template/*"]},
       include_package_data=True,
       packages=['InstantlyFiles',],)
