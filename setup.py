57f548bb93a1dd3d9d9fc79c357ed4a7b2552881
Timothy Crosley <timothy.crosley@gmail.com>
2013-02-15 19:32:55 -0500

    0.6.0: expand symbolic links when sharing templates

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@
 from distutils.core import setup
 
 setup(name='Instantly',
-      version='0.5.0',
+      version='0.6.0',
       description='A tool which allows you to create templates that can be expanded into full projects or stubs instantly.',
       author='Timothy Crosley',
       author_email='timothy.crosley@gmail.com',
       url='http://www.simpleinnovation.org/',
-      download_url='https://github.com/timothycrosley/Instantly/blob/master/dist/Instantly-0.5.0.tar.gz?raw=true',
+      download_url='https://github.com/timothycrosley/Instantly/blob/master/dist/Instantly-0.6.0.tar.gz?raw=true',
       license = "GNU GPLv2",
       scripts=['scripts/instantly',],
       requires = ['configobj', 'rest', 'webelements'],
