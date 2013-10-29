ff72b3253599b1bece98cd9e79af04caad5c4266
Timothy Crosley <timothy.crosley@gmail.com>
2013-10-29 18:39:08 -0400

    Release 0.8.1

--- setup.py
+++ setup.py
@@ -1,14 +1,21 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+from pies import *
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
 
 setup(name='Instantly',
-      version='0.8.0',
+      version='0.8.1',
       description='A tool which allows you to create templates that can be expanded into full projects or stubs instantly.',
       author='Timothy Crosley',
       author_email='timothy.crosley@gmail.com',
       url='http://www.simpleinnovation.org/',
-      download_url='https://github.com/timothycrosley/Instantly/blob/master/dist/Instantly-0.8.0.tar.gz?raw=true',
+      download_url='https://github.com/timothycrosley/Instantly/blob/master/dist/Instantly-0.8.1.tar.gz?raw=true',
       license = "GNU GPLv2",
       scripts=['scripts/instantly',],
       requires = ['configobj', 'rest', 'webelements'],
