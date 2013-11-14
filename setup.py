7a52f31e4a20085a154634dca27d347c0e3f2926
Timothy Crosley <timothy.crosley@gmail.com>
2013-11-14 01:02:44 -0500

    Update version of pies used

--- setup.py
+++ setup.py
@@ -2,22 +2,21 @@
 
 from __future__ import absolute_import, division, print_function, unicode_literals
 
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+from distutils.core import setup
+
+from pies.overrides import *
 
 setup(name='Instantly',
-      version='0.8.1',
+      version='0.8.3',
       description='A tool which allows you to create templates that can be expanded into full projects or stubs instantly.',
       author='Timothy Crosley',
       author_email='timothy.crosley@gmail.com',
       url='http://www.simpleinnovation.org/',
-      download_url='https://github.com/timothycrosley/Instantly/blob/master/dist/Instantly-0.8.1.tar.gz?raw=true',
+      download_url='https://github.com/timothycrosley/Instantly/blob/master/dist/Instantly-0.8.3.tar.gz?raw=true',
       license = "GNU GPLv2",
       scripts=['scripts/instantly',],
-      requires = ['configobj', 'rest', 'webelements'],
-      install_requires=['configobj', 'rest', 'webelements'],
+      requires=['configobj', 'rest', 'pies>=2.0.0'],
+      install_requires=['configobj', 'rest', 'pies>=2.0.0'],
       package_data={'InstantlyFiles': ["instant_templates/create_instant_template/*"]},
       include_package_data=True,
       packages=['InstantlyFiles',],)
