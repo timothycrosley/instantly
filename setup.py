fdb97bacb1e0ffd5a7f225e604c632998474392f
Timothy Crosley <timothy.crosley@gmail.com>
2013-11-17 20:22:01 -0500

    Update license, fix install script

--- setup.py
+++ setup.py
@@ -1,19 +1,18 @@
 #!/usr/bin/env python
 
-from __future__ import absolute_import, division, print_function, unicode_literals
-
-from distutils.core import setup
-
-from pies.overrides import *
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
 
 setup(name='Instantly',
-      version='0.8.3',
+      version='0.8.5',
       description='A tool which allows you to create templates that can be expanded into full projects or stubs instantly.',
       author='Timothy Crosley',
       author_email='timothy.crosley@gmail.com',
       url='http://www.simpleinnovation.org/',
-      download_url='https://github.com/timothycrosley/Instantly/blob/master/dist/Instantly-0.8.3.tar.gz?raw=true',
-      license = "GNU GPLv2",
+      download_url='https://github.com/timothycrosley/Instantly/blob/master/dist/Instantly-0.8.5.tar.gz?raw=true',
+      license = "MIT",
       scripts=['scripts/instantly',],
       requires=['configobj', 'rest', 'pies'],
       install_requires=['configobj', 'rest', 'pies>=2.0.0'],
