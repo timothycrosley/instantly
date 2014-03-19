05501d55c6ee0a5c3b3edbb3a3e86dd73cbea316
Timothy Crosley <timothy.crosley@gmail.com>
2014-03-19 05:24:19 -0400

    Fix setup to match new structure

--- setup.py
+++ setup.py
@@ -5,17 +5,44 @@ try:
 except ImportError:
     from distutils.core import setup
 
+try:
+   import pypandoc
+   readme = pypandoc.convert('README.md', 'rst')
+except (IOError, ImportError, OSError, RuntimeError):
+   readme = ''
+
 setup(name='Instantly',
       version='0.8.5',
-      description='A tool which allows you to create templates that can be expanded into full projects or stubs instantly.',
+      description='A tool which allows you to create templates that can be expanded into full projects or stubs '
+                  'instantly.',
+      long_description=readme,
       author='Timothy Crosley',
       author_email='timothy.crosley@gmail.com',
-      url='http://www.simpleinnovation.org/',
-      download_url='https://github.com/timothycrosley/Instantly/blob/master/dist/Instantly-0.8.5.tar.gz?raw=true',
+      url='http://www.instantly.pl/',
+      download_url='https://github.com/timothycrosley/instantly/archive/0.8.5.tar.gz',
       license = "MIT",
-      scripts=['scripts/instantly',],
       requires=['configobj', 'requests', 'pies'],
-      install_requires=['configobj', 'requests', 'pies>=2.0.0'],
-      package_data={'InstantlyFiles': ["instant_templates/create_instant_template/*"]},
-      include_package_data=True,
-      packages=['InstantlyFiles',],)
+      install_requires=['configobj', 'requests', 'pies>=2.6.1'],
+      packages=['instantly',],
+      entry_points={
+        'console_scripts': [
+            'instantly = instantly.main:main',
+        ]
+      },
+      keywords='Accelerate, Python, Python2, Python3, Templating, Automate, Template, Snippet',
+      classifiers=['Development Status :: 4 - Beta',
+                   'Intended Audience :: Developers',
+                   'Natural Language :: English',
+                   'Environment :: Console',
+                   'License :: OSI Approved :: MIT License',
+                   'Programming Language :: Python',
+                   'Programming Language :: Python :: 2',
+                   'Programming Language :: Python :: 2.6',
+                   'Programming Language :: Python :: 2.7',
+                   'Programming Language :: Python :: 3',
+                   'Programming Language :: Python :: 3.0',
+                   'Programming Language :: Python :: 3.1',
+                   'Programming Language :: Python :: 3.2',
+                   'Programming Language :: Python :: 3.3',
+                   'Topic :: Software Development :: Libraries',
+                   'Topic :: Utilities'],)
